7c6a1d79274eac3e9d56be5bd22a5542055dfa2a
Rinelle <yana.chaliullin@gmail.com>
2022-07-18 23:11:27 +0500

    Добавлена папка компонентов

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import './scss/main.scss';
+import { datepickers } from './js/components/datepickers';
 
 window.addEventListener('DOMContentLoaded', () => {
+    datepickers();
     // Все импортированные скрипты применять здесь
 });

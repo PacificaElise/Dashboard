b73adc202a3f81a662406a65a21d4c832ae6d3c3
Rinelle <yana.chaliullin@gmail.com>
2022-07-18 22:11:03 +0500

    Первоначальная структура проекта

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './scss/main.scss';
+
+window.addEventListener('DOMContentLoaded', () => {
+    // Все импортированные скрипты применять здесь
+});
---
+++ src/js/services/api/index.js
@@ -0,0 +1,7 @@
+class ApiService {
+    static send() {
+        // функция fetch
+    }
+}
+
+export default ApiService;

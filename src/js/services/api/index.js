85bf0152eabe56c465e7cfd430179ed70b1ded6f
RomanovaCute <romanova17romanova@yandex.ru>
2022-07-21 10:47:33 +0500

    start 'api service'

--- src/js/services/api/index.js
+++ src/js/services/api/index.js
@@ -1,6 +1,34 @@
 class ApiService {
-    static send() {
-        // функция fetch
+    static send(params) {
+        const {method = 'GET', data, url} = params,
+        host = '...'; //здесь будет ссылка
+
+        let encodeStr = '';
+
+        //получаем данные с сервера
+        if(method === 'POST' && data){
+            let values = [];
+            for(let key in data){
+                values.push(`${key}=${encodeURI(data[key])}`); //кодирование русских букв
+            }
+
+            encodeStr = `?${values.join('&')}`;
+        }
+
+        let requestData;
+
+
+        //отправляем данные на сервер
+        if(method === 'POST'){
+            requestData = JSON.stringify(data)
+        }
+
+        return fetch(`${host}${url ? `/${url}`: ''}${encodeStr}`, {
+            headers: {
+                'Content-type': 'json/application',
+                //.....
+            }
+        })
     }
 }
 

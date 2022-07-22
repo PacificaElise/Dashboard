0f0d67ae024ef49d0bf7cfa022200f59f381aa43
RomanovaCute <romanova17romanova@yandex.ru>
2022-07-22 11:02:10 +0500

    add fetch

--- src/js/services/api/index.js
+++ src/js/services/api/index.js
@@ -1,12 +1,12 @@
 class ApiService {
     static send(params) {
         const {method = 'GET', data, url} = params,
-        host = '...'; //здесь будет ссылка
+        host = 'dashboard.ri-nelly.ru/api';
 
         let encodeStr = '';
 
         //получаем данные с сервера
-        if(method === 'POST' && data){
+        if(method === 'GET' && data){
             let values = [];
             for(let key in data){
                 values.push(`${key}=${encodeURI(data[key])}`); //кодирование русских букв
@@ -15,20 +15,49 @@ class ApiService {
             encodeStr = `?${values.join('&')}`;
         }
 
-        let requestData;
 
+        let requestData;
 
         //отправляем данные на сервер
         if(method === 'POST'){
             requestData = JSON.stringify(data)
         }
 
-        return fetch(`${host}${url ? `/${url}`: ''}${encodeStr}`, {
+
+        const response = await fetch(`http://${host}/${url}${encodeStr}`, {
             headers: {
-                'Content-type': 'json/application',
-                //.....
-            }
+                'Content-type': 'json/application'
+            },
+            method,
+            data: requestData
         })
+        .catch(e => {
+            if (!e.response) {
+                return {
+                    netWorkError: true,
+                    message: 'Нет соединения с NestJs'
+                }
+            }
+            return e;
+        });
+
+        if (response.netWorkError) {
+            return response;
+        }
+
+        if ([200, 201].includes(response.status)) {
+            const result = await response.json();
+            return {
+                success: true,
+                data: result.data
+            }
+        }
+
+        return {
+            success: false,
+            message: await response.json()
+                .then(res => res.message)
+        }
     }
 }
 

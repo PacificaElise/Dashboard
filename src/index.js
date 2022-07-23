4c3452877fc37c023959f764d5ce75ca58586125
Tatiana Ivanova <tati.ivanova.87@gmail.com>
2022-07-23 19:31:45 +0300

    add select labels

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import './scss/main.scss';
 import { datepickers } from './js/components/datepickers';
+import { labels } from './js/components/labels';
+
 
 window.addEventListener('DOMContentLoaded', () => {
     datepickers();
+    labels();
+
     // Все импортированные скрипты применять здесь
 });
+

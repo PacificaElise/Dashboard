e1f13918e5ddb97f1e286f50c420dc961cc4b237
Ek-Al <kathrin1ma@gmail.com>
2022-07-24 17:26:31 +0300

    fixed 4 task

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import './scss/main.scss';
-//Подключаем наш модуль с функцией которая запускает наши датапикеры
 import { datepickers } from './js/components/datepickers';
-//Импортируем стили из node_modules Для наших датапикеров
 import 'simplepicker/dist/simplepicker.css';
 
 window.addEventListener('DOMContentLoaded', () => {
-    //Запускаем наш собственный модуль с нашими датапикерами
     datepickers();
-    // Все импортированные скрипты применять здесь
 });

4872ddc5c3208e638e375905620359aec34753f1
Rinelle <yana.chaliullin@gmail.com>
2022-07-25 21:26:26 +0500

    Поправлен datepicker

--- src/index.js
+++ src/index.js
@@ -1,11 +1,39 @@
 import './scss/main.scss';
-import { datepickers } from './js/components/datepickers';
+import {Datepicker} from './js/components/datepickers';
 import 'simplepicker/dist/simplepicker.css';
-import { labels } from './js/components/labels';
-import { navigations } from './js/components/nav';
+import {labels} from './js/components/labels';
+import {navigations} from './js/components/nav';
+import ApiService from "./js/services/api";
 
-window.addEventListener('DOMContentLoaded', () => {    
-    datepickers();
+window.addEventListener('DOMContentLoaded', () => {
+    // Datepicker для создания событий
+    new Datepicker({
+        el: '.timeDatepicker',
+        input: '.timeDatepicker__input',
+        onSubmit: (value) => {
+            console.log(value)
+        }
+    });
+
+    //Datepicker для отображения событий
+    new Datepicker({
+        el: '.theDatepicker',
+        input: '.theDatepicker__input',
+        options: {
+            disableTimeSection: true
+        },
+        onSubmit: (value) => {
+            // Тут должен быть твой метод который будет отображать данные в списке
+            ApiService.send({
+                url: 'event',
+                method: 'GET',
+                data: {
+                    day: value.getTime()
+                }
+            }).then(res => console.log(res))
+        }
+    });
+
     labels();
     navigations();
 

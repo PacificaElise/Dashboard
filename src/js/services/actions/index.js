53dbffc13c6417bfbb09f1de65d99c11c11e91ab
RomanovaCute <romanova17romanova@yandex.ru>
2022-07-28 14:33:14 +0500

    fix time format

--- src/js/services/actions/index.js
+++ src/js/services/actions/index.js
@@ -1,7 +1,4 @@
 import ApiService from '../api';
-
-
-
 class EventActions {
     static async remove(){
         ApiService.send({method: 'DELETE', data, url});
@@ -16,23 +13,49 @@ class EventActions {
             },
             url: 'event'
         })
-        .then(resp => console.log(resp))
-        .then(resp => {
-           
-            let tasksArr;
-            let str = '';   
+        .then(function(resp){
+            let tasksArr = resp.data,
+                plansList = document.querySelector('.business-today'),
+                plansContainer = document.querySelector('.plans'),
+                box = document.querySelector('.business-today_list')
 
-            tasksArr = resp.data;
             console.log(tasksArr);
+
             tasksArr.forEach(elem => {
-            return str += `<div class="details">
-                                <label class="details_time det-text-opacity">${elem.completeDate}</label>
+            
+            console.log(box);
+
+            const time = new Date(elem.completeDate);
+            console.log(time.getTime());
+
+            let hours, minutes, correctTime;
+            
+            function formatTime (time){
+                hours = String(time.getHours()).padStart(2,0),
+                minutes = String(time.getMinutes()).padStart(2,0)
+
+                correctTime = `${hours}:${minutes}`;
+            }
+
+            formatTime(time);
+
+            box.innerHTML += `<div class="details">
+                                <label class="details_time det-text-opacity">${correctTime}</label>
                                 <div class="details-label-string">
-                                    <span class="dot dot-color">${elem.markId}</span>
+                                    <span class="dot dot-color"></span>
                                     <p class="det-text-opacity">${elem.text}</p>
                                 </div>
-                                <div class="pic-close"></div>
+                                <div class="pic-close">
+                                <svg width="18" height="18" viewBox="0 0 18 18" fill="none"
+                                     xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                            d="M1.78125 17.5312L0.46875 16.2187L7.6875 9L0.46875 1.78125L1.78125 0.46875L9 7.6875L16.2187 0.46875L17.5312 1.78125L10.3125 9L17.5312 16.2187L16.2187 17.5312L9 10.3125L1.78125 17.5312Z"
+                                            fill="#FD4B33" />
+                                </svg>
+                            </div>
                             </div>`
+            plansList.append(box);
+            plansContainer.append(plansList);
             });
         })    
     }

b6f26ae8c4499f00f8fc0d09d90500a4832a8872
RomanovaCute <romanova17romanova@yandex.ru>
2022-07-29 15:48:12 +0500

    fix 'remove'

--- src/js/services/actions/index.js
+++ src/js/services/actions/index.js
@@ -2,6 +2,18 @@ import ApiService from '../api';
 
 
 class EventActions {
+    static async remove(idValue){
+        console.log(idValue);
+        ApiService.send({
+            method: 'DELETE', 
+            data: {
+                id: idValue
+            },
+            url: 'event'
+        })
+        .then(resp => console.log(resp))
+    }
+    
     static async getDayList(value){
         ApiService.send({
             method: 'GET',
@@ -20,56 +32,54 @@ class EventActions {
 
             tasksArr.forEach(elem => {
 
-            const time = new Date(elem.completeDate);
+                const time = new Date(elem.completeDate);
 
-            let hours, minutes, correctTime;
+                let hours, minutes, correctTime;
 
-            function formatTime (time){
-                hours = String(time.getHours()).padStart(2,0),
-                minutes = String(time.getMinutes()).padStart(2,0)
+                function formatTime (time){
+                    hours = String(time.getHours()).padStart(2,0),
+                    minutes = String(time.getMinutes()).padStart(2,0)
 
-                correctTime = `${hours}:${minutes}`;
-            }
+                    correctTime = `${hours}:${minutes}`;
+                }
 
-            formatTime(time);
+                formatTime(time);
             
-            const details = document.createElement('div');
-            details.setAttribute('class', 'details');
+                const details = document.createElement('div');
+                details.setAttribute('class', 'details');
 
-            details.innerHTML += `
+                details.innerHTML += `
                                 <label class="details_time det-text-opacity">${correctTime}</label>
                                 <div class="details-label-string">
                                     <span class="dot dot-color"></span>
                                     <p class="det-text-opacity">${elem.text}</p>
                                 </div>
-                                <div class="pic-close">
-                                <svg width="18" height="18" viewBox="0 0 18 18" fill="none"
+                                
+                                <svg class="pic-close" data-id=${elem.id} width="18" height="18" viewBox="0 0 18 18" fill="none"
                                      xmlns="http://www.w3.org/2000/svg">
                                     <path
                                             d="M1.78125 17.5312L0.46875 16.2187L7.6875 9L0.46875 1.78125L1.78125 0.46875L9 7.6875L16.2187 0.46875L17.5312 1.78125L10.3125 9L17.5312 16.2187L16.2187 17.5312L9 10.3125L1.78125 17.5312Z"
                                             fill="#FD4B33" />
                                 </svg>
-                            </div>`
+                            `
                 box.append(details)
                 plansList.append(box);
                 plansContainer.append(plansList);
             });
 
-            // let deleteBtn = document.querySelectorAll('.pic-close');
-            document.addEventListener('click', (e) => {
-                ApiService.send({
-                    method: 'DELETE', 
-                    data: {
-                        id: tasksArr[0].id
-                    },
-                    url: 'event'
+            const deleteBtn = document.querySelectorAll('.pic-close');
+
+            deleteBtn.forEach(item => {
+                item.addEventListener('click', (e) => {
+                    let target = e.currentTarget
+                    let idValue = target.getAttribute('data-id')
+                    console.log(idValue);
+                    EventActions.remove(idValue)
                 })
-                .then(resp => console.log(resp))
             })
-
-        })    
-    }
-
+            })
+        }
+}
 
     // static async update(){
     //     // send({method: 'GET', data, url});
@@ -77,7 +87,7 @@ class EventActions {
     //     getDayList(); //получаем данные по выбранной дате
     //     rebuildList();     //обновляем верстку
     // }
-}
+
 
 
 

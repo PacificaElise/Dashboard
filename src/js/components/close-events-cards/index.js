7db019d9ac7dccfc3383714d40ddacb1d2665544
Tatiana Ivanova <tati.ivanova.87@gmail.com>
2022-07-30 17:12:25 +0300

    add changes

--- src/js/components/close-events-cards/index.js
+++ src/js/components/close-events-cards/index.js
@@ -1,4 +1,5 @@
 import ApiService from "../../services/api";
+import Ellipsis from "../../../../node_modules/ellipsis.js";
 
 function getClosestEvents() {
 
@@ -7,44 +8,27 @@ new ApiService.send({
     method: 'GET',
 })
 .then(res => res.data)
-.then(res => console.log (res))
-.then (res => {
-
-    //let data = res;
-    let data = [
-        {id:1, text:'Текстовое сообщение 1', completeDate:'2022-07-26T07:00:00.000Z', mark:null, isExpired:true},
-        {id:2, text:'Текстовое сообщение 2', completeDate:'2022-07-28T10:15:00.000Z', mark:{id:'2', color:'#F0A7C4', title:'Учёба'}, isExpired:false},
-        {id:3, text:'Текстовое сообщение 3', completeDate:'2022-07-27T13:48:00.000Z', mark:{id:'4', color:'#85CF6B', title:'Отдых'}, isExpired:false},
-        {id:4, text:'Текстовое сообщение 4', completeDate:'2022-07-26T16:58:00.000Z', mark:null, isExpired:false},
-        {id:5, text:'Текстовое сообщение 5', completeDate:'2022-07-28T10:30:00.000Z', mark:null, isExpired:false},
-    ];
+.then (res => {   
 
         
-    let notExpired = data.filter(data => data.isExpired == false);
-    let parseDates = notExpired.map(d => Date.parse(d.completeDate));
-    let result = parseDates.sort().reverse().splice(0,3);
+    let notExpired = res.filter(data => !data.isExpired);
+    notExpired.forEach(item => {
+        item.completeDate = new Date(item.completeDate).toLocaleString()
+    });
+    let result = notExpired.sort().splice(0,3);
     
     const closestEvents = document.querySelector('.close-events-cards');
 
-    for (let i = 0; i < result.length ; i++) {
-        for (let d = 0; d < notExpired.length; d++) {
-            if (Date.parse(notExpired[d].completeDate) === result[i]) {
-                    closestEvents.innerHTML += `<div class="closest-event">
-                    <div class="date">${notExpired[d].completeDate.slice(0,-8).replace(/\T/, ' ').replace(/\-/g, '.')}</div>
-                    <div class="content">${notExpired[d].text}</div>
-                    <div class="label" data-id ="${notExpired[d].id}"</div>
-                </div>`;
-        }
-    }} 
+    result.forEach(item => {
+        closestEvents.innerHTML += `<div class="closest-event" class="clamp">
+        <div class="date">${item.completeDate}</div>
+        <div class="content">${item.text}</div>
+        <div class="label" style="background: ${item.mark.color}"</div>
+    </div>`;    
+    });
 
-    let labelId = document.querySelector('.label').dataset.id;
+    Ellipsis();
 
-    for (let d = 0; d < notExpired.length; d++) {
-        if (notExpired[d].mark !== null) {
-        if (+labelId === notExpired[d].id) { 
-            document.querySelector('.label').style.background = notExpired[d].mark.color
-        }}
-    }
     })
 }
 

24dadc3ed03bf68ae64ed6a9104ce877c98dc898
Tatiana Ivanova <tati.ivanova.87@gmail.com>
2022-07-31 16:22:01 +0300

    final version closest events

--- src/js/components/close-events-cards/index.js
+++ src/js/components/close-events-cards/index.js
@@ -10,30 +10,25 @@ new ApiService.send({
 .then (res => {   
 
 
-    /*let data = [
-    {id: 3, text: 'Тестовое событие 3', completeDate: '2022-07-26T10:00:09.000Z', mark: {id: 1, title: 'Важное', color: '#f0520e'}, isExpired: false},
-    {id: 4, text: 'asdad', completeDate: '2022-07-27T13:28:00.000Z', mark: null, isExpired: false},
-    {id: 5, text: 'sasda', completeDate: '2022-07-27T13:29:00.000Z',ark: {id: 2, title: 'Важное', color: '#DFE1E5'}, isExpired: false}
-    ];*/
-
     let notExpired = res.filter(data => !data.isExpired);
     notExpired.forEach(item => {
         item.completeDate = new Date(item.completeDate).toLocaleString()
     });
     let result = notExpired.sort().splice(0,3);
     console.log(result)
-    
+
+    let withColor = result.filter(color => !!color.mark)
+    console.log(withColor)
+
     const closestEvents = document.querySelector('.close-events-cards');
-    let labels;
+    let cards;
 
     result.forEach (item => {
         closestEvents.innerHTML += `<div class="closest-event">
         <div class="date">${item.completeDate}</div>
         <div class="content">${item.text}</div>
-        <div class="label" data-id = "${item.id}" style="background: ${item.mark.color}"</div>
+        ${item.mark ? `<div class="label" style="background:${item.mark.color}"></div>` : ''}
     </div>`; 
-    labels = document.querySelector(`[data-id="${item.id}"]`);
-    console.log(labels.dataset.id)   
     });
     })
 }
